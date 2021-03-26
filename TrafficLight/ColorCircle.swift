ebea95c1797d2bd8dd791a6fbaadd79c2a13c6f9
Nikita Zharinov <nikita.zharinov@gmail.com>
2021-03-26 17:00:27 +0100

    Circles and Button

---
+++ TrafficLight/ColorCircle.swift
@@ -0,0 +1,28 @@
+//
+//  ColorCircle.swift
+//  TrafficLight
+//
+//  Created by Nikita Zharinov on 26/03/2021.
+//
+
+import SwiftUI
+
+struct ColorCircle: View {
+    let color: Color
+    let opacity: Double
+    var body: some View {
+        Circle()
+            .foregroundColor(color)
+            .frame(width: 100, height: 100)
+            .clipShape(Circle())
+            .opacity(opacity)
+            .overlay(Circle().stroke(Color.white, lineWidth: 5))
+            
+    }
+}
+
+struct ColorCircle_Previews: PreviewProvider {
+    static var previews: some View {
+        ColorCircle(color: .red, opacity: 1.0)
+    }
+}
--- TrafficLight/ContentView.swift
+++ TrafficLight/ContentView.swift
@@ -8,11 +8,61 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    enum light {
+        case red
+        case yellow
+        case green
+    }
+    
+    @State private var buttonPressed = false
+    @State private var buttonTitle = "START"
+    @State private var currentLight = Color(.red)
+    
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Color(.black)
+                .ignoresSafeArea()
+            VStack {
+                
+                
+                ColorCircle(color: .red, opacity: 0.3)
+
+                ColorCircle(color: .yellow, opacity: 0.3)
+
+                ColorCircle(color: .green, opacity: 0.3)
+                    
+                Spacer()
+                Button(action: { PressedButton() }, label: {
+                    Text(buttonTitle)
+                        .frame(width: 170, height: 50)
+                        .font(.largeTitle)
+                        .foregroundColor(.white)
+                        .background(Color(.blue))
+                        .overlay(RoundedRectangle(cornerRadius: 15).stroke(Color.white, lineWidth: 5))
+                })
+                .padding()
+            }.padding()
+        }
+    }
+    private func PressedButton() {
+        if !buttonPressed {
+            buttonPressed.toggle()
+            buttonTitle = "NEXT"
+        }
+//        switch currentLight {
+//        case .red:
+//            <#code#>
+//        case .yellow:
+//            <#code#>
+//        case .green:
+//            <#code#>
+//        }
+        
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
